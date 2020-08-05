467a79342d9f4b315bcd214eb67b0a4e2bfb3329
Michael Saxon <mssaxon@asu.edu>
2020-08-04 22:21:33 -0700

    updated publications and style of publications page

--- resize.js
+++ resize.js
@@ -2,3 +2,12 @@ function resizeleft() {
 var rh = document.getElementById('rightcontent').offsetHeight;
 document.getElementById('leftcontent').style.height=rh+ 'px';
 }
+
+function showhideattrib() {
+ var x = document.getElementById("attrib");
+ if (x.style.display === "none") {
+   x.style.display = "block";
+ } else {
+   x.style.display = "none";
+ }
+}
